4400a68258a95210e9e5d0f67127d4533cd29044
otles <otles@otles-Precision-M2400>
2016-01-18 23:22:34 -0600

    new files added

---
+++ lib/uRProgramming/R/LoopStructures.R
@@ -0,0 +1,32 @@
+## Copyright (C) 2009 Frontier Science & Technology
+##
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by the
+## Free Software Foundation; either version 2, or (at your option) any
+## later version.
+##
+## These functions are distributed in the hope that they will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## The text of the GNU General Public License, version 2, is available
+## as http://www.gnu.org/copyleft or by writing to the Free Software
+## Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+
+#######################################
+# Function FUNCTION_NAME
+#######################################
+#The purpose of these function to
+#functionality
+#This function written by FIRST AND LAST NAME <e-mail address>
+#Date of function first written
+
+FUNCTION_NAME<-function(mat=freqData,xlabels=NULL,ylabels=NULL,onePlot=FALSE,besideOrNot=TRUE,isCol=TRUE,titleOfPlot=NULL){
+#Write the contents of R
+{
+
+}
+
