9ee9811ad4e2ec6ddd3ee36e8dad12541cba5748
zekai otles <zekaiotles@aim.com>
2016-02-06 01:15:45 -0600

    rename two files

---
+++ lib/uRProgramming/R/loopStructures.R
@@ -0,0 +1,30 @@
+##
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by the
+## Free Software Foundation; either version 2, or (at your option) any
+## later version.
+##
+## These functions are distributed in the hope that they will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## The text of the GNU General Public License, version 2, is available
+## as http://www.gnu.org/copyleft or by writing to the Free Software
+## Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+
+#######################################
+# Function FUNCTION_NAME
+#######################################
+#The purpose of these function to
+#functionality
+#This function written by FIRST AND LAST NAME <e-mail address>
+#Date of function first written
+
+loopStructures<-function(){
+#Write the contents of R
+{
+
+}
