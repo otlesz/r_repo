419d5bddf28d7fdca0f6eaf8e2d0b49aa7dc2b87
zekai otles <zekai otles>
2016-10-26 01:47:02 -0500

    minor change

--- test/testDataTypes.R
+++ test/testDataTypes.R
@@ -4,22 +4,25 @@ testDataTypes<-function(){
   #create logical variable
   iTest<-T
   is.logical(iTest)
-  
-  if(iTest ==  is.logical(iTest)) cat("Logical \n")
+  cat(iTest)
+  if(iTest ==  is.logical(iTest)) cat("\n Logical \n")
   
 #create simple double vector 
   x<-seq(1,100,by=10.011)
+  cat(x)
   is.double(x)
-  if(iTest ==  is.double(x)) cat("x is double Vector \n")
+  if(iTest ==  is.double(x)) cat("\n x is double Vector \n")
   
   #cast double vector to integer 
   y<-as.integer(x)
+  cat(y)
   is.integer(y)
-  if(iTest ==  is.integer(y)) cat("y is integer Vector \n")
+  if(iTest ==  is.integer(y)) cat("\n y is integer Vector \n")
   
   #cast to character vector
   
   z<-as.character(x)
+  cat(z)
   is.character(z)
-  if(iTest ==  is.character(z)) cat("z is character Vector \n")
+  if(iTest ==  is.character(z)) cat("\n z is character Vector \n")
 }
