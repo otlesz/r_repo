b19408010c1590dfdaecd1a7194901249a34a6c4
zekai otles <zekai otles>
2016-10-26 01:47:02 -0500

    switch statement added

--- test/usingSwitch.R
+++ test/usingSwitch.R
@@ -1,25 +1,11 @@
-#Simple example for data types
-testDataTypes<-function(){
+#Simple example for data types casting using swith statement
+usingSwitch<-function(x,dataTypes){
   
-  #create logical variable
-  iTest<-T
-  is.logical(iTest)
+  switch (dataTypes,
+    integer = {x<-as.integer(x)},
+    double = {x<-as.doubles(x)},
+    character = {x<-as.character(x)}
   
-  if(iTest ==  is.logical(iTest)) cat("Logical \n")
+  )
   
-#create simple double vector 
-  x<-seq(1,100,by=10.011)
-  is.double(x)
-  if(iTest ==  is.double(x)) cat("x is double Vector \n")
-  
-  #cast double vector to integer 
-  y<-as.integer(x)
-  is.integer(y)
-  if(iTest ==  is.integer(y)) cat("y is integer Vector \n")
-  
-  #cast to character vector
-  
-  z<-as.character(x)
-  is.character(z)
-  if(iTest ==  is.character(z)) cat("z is character Vector \n")
 }
