1c6466ae5788d5514b0f38570942bfa1e8de61aa
zekai otles <zekai otles>
2016-11-15 23:17:22 -0600

    is.na replaced with 999999

--- test/insertData.R
+++ test/insertData.R
@@ -10,17 +10,9 @@ numR<-dim(airquality)[1]
 for (iRow in 1:numR) {
 #  print(iRow)
   data<-airquality[iRow,]
-  print(paste(iRow,"\n"))
-  print(data)
-  for (indx in 1:dim(data)[2]){
-    print(paste(indx,"\n"))
-    if(is.na(data[indx])){
-      print(data[indx])
-      data[indx]<-NULL
-      print(data[indx])
-      }
-   }
-  statement<-paste("INSERT INTO ","airquality")
+  
+ data[is.na(data)]<-999999
+   
 #  print(statement)
   insertQuery <- paste(paste(statement,"VALUES("), paste(data, collapse = ", "), ")")
 #  
