b42055937d0c71a16b26bc06e92e2bf6008313e5
zekai otles <zekai otles>
2017-01-29 01:49:56 -0600

    first time checkin

---
+++ test/formTempMatrix.R
@@ -0,0 +1,24 @@
+#form a Matrix from dataFrm data
+#the purpose of this function to ceate Temp matrix
+formTempMatrix<-function(dataFrm)
+{
+  monthAsNum<-unique(dataFrm$Month)
+  row_Names<-array("",dim=length(unique(dataFrm$Month)))
+  tempAsMatrix<-matrix("",nrow=length(unique(dataFrm$Month)),ncol=30)
+  
+  
+  for(mIndx in 1:length(monthAsNum)){
+    row_Names[mIndx]<-switch(as.character(monthAsNum[mIndx]),"5"="May","6"="June","7"="July","8"="Aug","9"="Sep")
+  }
+  row.names(tempAsMatrix)<-row_Names
+    
+  length(unique(dataFrm$Month))
+  rowIndex<-unique(dataFrm$Month)
+  for (index in 1:length(rowIndex)){
+    
+    tempAsMatrix[index,]<-dataFrm$Temp[dataFrm$Month==rowIndex[index]]
+    
+  }
+ 
+  return(tempAsMatrix)
+}
