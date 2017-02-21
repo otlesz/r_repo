d723c761dd6ca0c07b1a58be0966500149eda312
zekai otles <zekai otles>
2017-02-20 23:11:49 -0600

    Code for random NumGen is completed.

--- test/randomNumGen.R
+++ test/randomNumGen.R
@@ -1,23 +1,30 @@
 #Create randomly number generated vectors
 #simple statistics
 randomNumGen<-function(seed=FALSE){
- 
+ print("Normally distributed Random Number")
   for(indx in 0:3){
+    print(paste("Iteration ",indx,sep= ""))
     if(seed) set.seed(6543)
     xTempNorm<-rnorm(100,273.16,15)
-  
-    summary(xTempNorm)
-    print(xTempNorm[5])
+
+    print(paste("xTempNorm[5]=",xTempNorm[5]))
+    cat('summary(xTempNorm)\n')
+    print(summary(xTempNorm))
+    
   }
   
+  print("Uniformly distributed Random Number")
   for(indx in 0:3){
     if(seed)set.seed(6543)
     xTempUni<-runif(100,240,320)
-    print(xTempUni[5])
+    print(paste("Iteration ",indx,sep= ""))
+    print(paste("xTempUni[5]=",xTempUni[5]))
+    cat('summary(xTempUni)\n')
+    #
     
-    summary(xTempUni)
+    print(summary(xTempUni))
     
   }
   
-  return(list(xTempNorm=xTempNorm,xTempUni=xTempUni))
+#  return(list(xTempNorm=xTempNorm,xTempUni=xTempUni))
 }
