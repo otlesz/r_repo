e579259bf5592475325c0916f42248d166045c3b
LAPTOP-KG8AVOII\otles <otlesz@gmail.com>
2017-09-06 00:54:06 -0500

    tryting to create sparse data and runt-test on int

--- test/paired_tTest.R
+++ test/paired_tTest.R
@@ -2,3 +2,17 @@ library('uRProgramming')
 heightData<-data.frame(CreateDataFile())
 df<-heightData
 t.test(df$Kadin,df$Erkek,paired=TRUE, conf.level=0.95)
+
+#Do it for the chickwts (not equal number of sample)
+#data("chickwts")
+
+colNames<-levels(chickwts$feed)
+
+
+for (colIndx in 1:length(colNames)){
+  x<-chickwts[which(as.character(chickwts$feed)==as.character(colNames[colIndx])),]
+  if(colIndx == 1)
+   dfNew<-x[1]
+  else
+    dfNew<-cbind(x[1])
+}
