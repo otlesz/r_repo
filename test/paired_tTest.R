c745b8eb07f4df812aa94323a89ac519c95700b2
LAPTOP-KG8AVOII\otles <otlesz@gmail.com>
2017-09-07 00:02:26 -0500

    Turn it to the function and created data structure, so chicken weights can be compared.

--- test/paired_tTest.R
+++ test/paired_tTest.R
@@ -1,3 +1,5 @@
+paired_tTest<-function(){
+
 library('uRProgramming')
 heightData<-data.frame(CreateDataFile())
 df<-heightData
@@ -7,12 +9,17 @@ t.test(df$Kadin,df$Erkek,paired=TRUE, conf.level=0.95)
 #data("chickwts")
 
 colNames<-levels(chickwts$feed)
-
-
+xMatr<-array(NA,c(14,6))
+colnames(xMatr)<-colNames
 for (colIndx in 1:length(colNames)){
   x<-chickwts[which(as.character(chickwts$feed)==as.character(colNames[colIndx])),]
-  if(colIndx == 1)
-   dfNew<-x[1]
-  else
-    dfNew<-cbind(x[1])
+  x1<-x$weight
+  #print("x",x1)
+  for (rowIndx in 1:length(x1)){
+    
+    xMatr[rowIndx,colIndx]<-x1[rowIndx]
+  }
+  
+}
+return(xMatr)
 }
