10ef401f8003cae73cddd6e58ac866c1063c7e33
zekai otles <otlesz@gmail.com>
2018-02-22 00:00:09 -0600

    update to print table with multiple column

--- test/paired_tTest.R
+++ test/paired_tTest.R
@@ -12,8 +12,12 @@ t.test(df$Kadin,df$Erkek,paired=TRUE, conf.level=0.95)
 colNames<-levels(chickwts$feed)
 xMatr<-array(NA,c(14,6))
 
-xStats<-array("",c(length(colnames),6))
+maxLen<-dim(xMatr)[2]-1
+tvalues<-array("",c(maxLen,dim(xMatr)[2]))
 
+
+xStats<-array("",c(length(colNames),7))
+tValues<-array("",c(6,3))
 
 pValues<-array("",c(5,6))
 
@@ -29,31 +33,52 @@ for (colIndx in 1:length(colNames)){
   
 }
 
-maxLen<-dim(xMatr)[2]-1
+
 
 for (rrowIndx in 1:maxLen){
   iStart<-rrowIndx+1
+  
   for (ccolIndx in iStart:dim(xMatr)[2]){
     
-    tvalues[rowIndx,ccolIndx]<-t.test(xMatr[,rrowIndx],xMatr[,ccolIndx],paired=TRUE, conf.level=0.95)
-    pvals<-tvalues[rowIndx,ccolIndx]$p.value
-
+   # tvalues[rowIndx,ccolIndx]
+    tValue<-t.test(xMatr[,rrowIndx],xMatr[,ccolIndx],paired=TRUE, conf.level=0.95)
+    #pvals<-tvalues[rowIndx,ccolIndx]$p.value
+    pvals<-tValue$p.value
     pValues[rrowIndx,ccolIndx]<-format(pvals,digits = 1 ,nsmall = 5, justify = "left")
 
+      rowValues<-paste("rrowIndx=",rrowIndx,sep=" ")
+      colValues<-paste("ccolIndx=",ccolIndx,sep=" ")
+    print (rowValues)
     print (pValues[rrowIndx,ccolIndx])
   }
+  tValues[iStart,1]<-format(tValue$parameter,digits = 2 ,nsmall = 0, justify = "left")
+  tValues[iStart,2]<-format(tValue$statistic,digits = 2 ,nsmall = 4, justify = "left")
+  tValues[iStart,3]<-format(tValue$p.value,digits = 1 ,nsmall = 4, justify = "left")
+  
 }
 
 
-for (rr2owIndx in 1:maxLen){
+
+
   for (columnIndx in 1:dim(xMatr)[2]){
-    xStats[rr2owIndx,1]<-length(xMatr[,ccolIndx])
-    xStats[rr2owIndx,2]<-summary(xMatr[,ccolIndx])[4]
-    xStats[rr2owIndx,3]<-summary(xMatr[,ccolIndx])[7]
-    xStats[rr2owIndx,4]<-sd(xMatr[,ccolIndx])
-   
-  }
-}
+ #   print(c("columnIndx=",columnIndx))
+    rr2owIndx=columnIndx
+ #   print(xStats)
+    xStats[rr2owIndx,1]<-length(xMatr[,columnIndx])
+    xStats[rr2owIndx,2]<-summary(xMatr[,columnIndx])[4]
+    xStats[rr2owIndx,3]<-summary(xMatr[,columnIndx])[7]
+    xStats[rr2owIndx,4]<-format(sd(xMatr[,columnIndx],na.rm=TRUE),digits = 2 ,nsmall = 2, justify = "left")
+    if(rr2owIndx >=2) {
+      xStats[rr2owIndx,5]<-tValues[rr2owIndx,1]
+      xStats[rr2owIndx,6]<-tValues[rr2owIndx,2]
+      xStats[rr2owIndx,7]<-tValues[rr2owIndx,3]
+    }
+#    print(xStats)
+    rownames(xStats)<-colNames
+    colnames(xStats)<-c("n","Mean","num Of NA","StdDev","df","t","p-values")
+ 
+} #columnIndx
+
 
 
 
